460375faaf39cd4c3a0c957b10944113e34a9ff6
Dustin Reff <Dustin.Reff@mines.sdsmt.edu>
2021-04-21 10:28:28 -0600

    got my login info off of git

--- www/videogameconfig.php
+++ www/videogameconfig.php
@@ -2,8 +2,8 @@
 
 $host="127.0.0.1"; //hostname URL
 $port=3306;						//default port 3306
-$user="root";					//DBMS login username
-$password="SteveCat13@*";				//DBMS login password
+$user="change_me";					//DBMS login username
+$password="change_me";				//DBMS login password
 $dbname="mydb";		//Select DB 
 
 ?> 
